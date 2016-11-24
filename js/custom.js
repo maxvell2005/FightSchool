177ad045d54121074539fe82cd24747cefae2f2d
v.elin <v.elin@ukrcard.com.ua>
2016-11-24 15:39:41 +0200

    Final version yet, need clarification

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,19 @@
 /**
  * Created by v.elin on 11/21/2016.
  */
-$('.grid').isotope({
-    // options
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
+// $('.grid').isotope({
+//     options
+//     itemSelector: '.grid-item',
+//     layoutMode: 'fitRows'
+// });
+
+$(document).ready(function () {
+
+    $('.grid').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'masonry'
+    });
+
+
 });
